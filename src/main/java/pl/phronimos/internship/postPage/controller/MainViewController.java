fdec8897fc4557d45f627236c7d4b49c1d071cc2
cristofp <krzysiekplachno@gmail.com>
2019-10-08 15:54:25 +0200

    Clapping served from backend added

--- src/main/java/pl/phronimos/internship/postPage/controller/MainViewController.java
+++ src/main/java/pl/phronimos/internship/postPage/controller/MainViewController.java
@@ -4,13 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import pl.phronimos.internship.postPage.service.PostService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MainViewController {
 
+    public MainViewController() {
+        clapsMap.put(1, 3);
+        clapsMap.put(2, 10);
+    }
+
     @Autowired
     private PostService postService;
+    private final HashMap<Integer, Integer> clapsMap = new HashMap<>();
+
+    @ModelAttribute("clapsMap")
+    public Map<Integer, Integer> getClapsMap() {
+        return clapsMap;
+    }
+
 
     @GetMapping(value = {"/mainView", "/"})
     public String mainView(Model model) {
